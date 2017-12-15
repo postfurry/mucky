62f16f341f4e5527079319164be07cb4f3264cb2
Morgan Nichols <morgan@latrani.net>
2017-12-14 21:45:18 -0800

    Format activity timestamps nicely
    Closes #12

--- frontend/components/OutputLine.js
+++ frontend/components/OutputLine.js
@@ -1,16 +1,19 @@
 import { h, Component } from 'preact'
 import classNames from 'classnames'
 import Linkify from 'react-Linkify'
+import moment from 'moment'
 
 export default class OutputLine extends Component {
   constructor(props) {
     super(props)
     this.noAnsiData = this.props.data.replace(/\[((\d*);){0,2}(\d*)m/g, '')
+
+    this.timestamp = moment(this.props.timestamp).format('h:mm:ssa MMM DD')
   }
   render() {
     const className = classNames('output-line', this.props.type)
 
-    return (<div className={className} title={this.props.timestamp}>
+    return (<div className={className} title={this.timestamp}>
       <Linkify>{this.noAnsiData}</Linkify>
     </div>)
   }
